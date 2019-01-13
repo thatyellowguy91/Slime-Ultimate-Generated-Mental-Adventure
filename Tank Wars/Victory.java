8e35c8c39d2156fa216a3ffa77f00693df57bee6
thatyellowguy91 <32936024+thatyellowguy91@users.noreply.github.com>
2019-01-12 20:16:08 -0800

    bad

--- Tank Wars/Victory.java
+++ Tank Wars/Victory.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Tulisan extends Actor
+public class Victory extends Actor
 {
     /**
      * Act - do whatever the Tulisan wants to do. This method is called whenever
