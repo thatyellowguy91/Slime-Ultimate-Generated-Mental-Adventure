bd768fe5070b7ca62835b1a25c125b6de942401c
thatyellowguy91 <32936024+thatyellowguy91@users.noreply.github.com>
2019-01-13 22:24:35 -0800

    Jellyay

---
+++ JellySimulator/Car.java
@@ -0,0 +1,111 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+/**
+ * Write a description of class spring here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Car  extends Actor
+{
+    GreenfootImage screen = new GreenfootImage(600, 400);
+    double[] x = {225,275,275,225};
+    double[] y = {225,225,275,275};
+    double[] xv = {0,0,0,0};
+    double[] yv = {0,0,0,0};
+    double a,b,c,d,l,ang;
+    int[] conect1 = {0,1,2,3,0,1}; //0,1,2,3,0,1 for sq
+    int[] conect2 = {1,2,3,0,2,3}; //1,2,3,0,2,3 for sq
+    double[] dl = new double[6];
+
+    public Car(){
+        setImage(screen);
+        for(int f=0;f<6;f++){
+            dl[f] = Math.sqrt(Math.pow((x[conect1[f]] - x[conect2[f]]),2)+Math.pow((y[conect1[f]] - y[conect2[f]]),2));
+        }
+   }
+    
+    
+    
+    public void act() 
+    {
+        screen.clear();
+        for(int f=0;f<6;f++)
+        {
+            ((JellyWorld) getWorld()).isGround((int)(x[conect1[f]]),(int)(y[conect1[f]]),(int)(x[conect2[f]]),(int)(y[conect2[f]]));
+            screen.setColor(Color.RED);
+            screen.drawLine((int)(x[conect1[f]]),(int)(y[conect1[f]]),(int)(x[conect2[f]]),(int)(y[conect2[f]]));
+        }
+        
+        for(int f=0;f<4;f++){
+            
+             if(((JellyWorld) getWorld()).isGround==false)
+             {
+                yv[f] = yv[f]+.2;
+            }
+            
+            if(((JellyWorld) getWorld()).isGround==true)
+             {
+                yv[f] = yv[f]-.3;
+            }
+
+            if(Greenfoot.isKeyDown("right"))
+                xv[f] = xv[f]+.2;
+            if(Greenfoot.isKeyDown("left"))
+                xv[f] = xv[f]-.2;
+            if(Greenfoot.isKeyDown("up") && ((JellyWorld) getWorld()).isGround==true)
+            {
+                yv[f] = - 7;
+                y[f] = y[f] - 7;
+            }
+        }
+        
+        for(int f=0;f<6;f++){
+            l = Math.sqrt(Math.pow((x[conect1[f]] - x[conect2[f]]),2)+Math.pow((y[conect1[f]] - y[conect2[f]]),2));
+            l = dl[f]-l;
+            l=l/10.0;
+            ang = Math.atan2(y[conect1[f]]-y[conect2[f]],x[conect1[f]]-x[conect2[f]]);
+            xv[conect1[f]] = xv[conect1[f]] + l*Math.cos(ang);
+            xv[conect2[f]] = xv[conect2[f]] - l*Math.cos(ang);
+            
+//             if(((JellyWorld) getWorld()).isGround==false)
+//             {
+                yv[conect1[f]] = yv[conect1[f]] + l*Math.sin(ang);
+                yv[conect2[f]] = yv[conect2[f]] - l*Math.sin(ang);
+//            }
+        }
+        
+        for(int f=0;f<4;f++){
+            
+            xv[f] = .99*xv[f];
+            x[f] = x[f] + xv[f];
+            yv[f] = .99*yv[f];
+            
+                y[f] = y[f] + yv[f];
+            
+            if(y[f]>399){
+                yv[f] = 0;
+                y[f]=399;
+                xv[f]=0;
+            }
+            if(y[f]<0){
+                yv[f] = 0;
+                y[f]=0;
+                xv[f]=0;
+            }
+            if(x[f]<0){
+                yv[f] = 0;
+                x[f]=0;
+                xv[f]=0;
+            }
+            if(x[f]>599){
+                yv[f] = 0;
+                x[f]=599;
+                xv[f]=0;
+            }
+        }
+        
+    }    
+}
+
---
+++ JellySimulator/JellyWorld.java
@@ -0,0 +1,57 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+/**
+ * Write a description of class JellyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class JellyWorld  extends World
+{
+
+    public boolean isGround = false;
+    GreenfootImage background = new GreenfootImage("background1.png");
+    GreenfootImage mask = new GreenfootImage("mask1.png");
+    GreenfootImage car = new GreenfootImage("mask1.png");
+
+    /**
+     * Constructor for objects of class JellyWorld.
+     * 
+     */
+    public JellyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1, false); 
+
+        addObject(new Car(), 300, 200);
+    }
+    
+    public void act()
+    {
+        setBackground(background);
+        car.setColor(Color.WHITE);
+        car.fillRect(0,0,600,400);
+    }
+    
+    public void isGround(int a, int b, int c, int d)
+    {
+        if(d<398 && b<398)
+        {
+            if(
+            mask.getColorAt(c,d+2).equals(new Color(0,0,0)) ||
+            mask.getColorAt(a,b+2).equals(new Color(0,0,0)) ||
+            mask.getColorAt(a,d+2).equals(new Color(0,0,0)) ||
+            mask.getColorAt(c,b+2).equals(new Color(0,0,0)) ||
+            mask.getColorAt(b,d+2).equals(new Color(0,0,0)) ||
+            mask.getColorAt(d,b+2).equals(new Color(0,0,0)))
+            {
+                isGround = true;
+            }
+            else
+            {
+                isGround = false;
+            }
+        }
+    }
+}
