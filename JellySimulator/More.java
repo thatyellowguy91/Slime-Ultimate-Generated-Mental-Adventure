9f3994477ac83bb86a608c9ca4ed881c24093e2e
thatyellowguy91 <32936024+thatyellowguy91@users.noreply.github.com>
2019-01-14 01:05:57 -0800

    Kashoot me fam

--- JellySimulator/More.java
+++ JellySimulator/More.java
@@ -12,9 +12,9 @@ public class More extends Actor
      * Act - do whatever the More wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void more() 
+    public void act() 
     {
        if (Greenfoot.mouseClicked(this)) 
-            getWorld().addObject(new JellyBlock(),300,200);       
+            getWorld().addObject(new JellyBlock(),300,200);
     }
 }
